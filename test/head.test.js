1f4002d3c96d60ec71c49ef93358067aa21c36e3
abdmmn <86195719+abdmmn@users.noreply.github.com>
2023-02-19 23:48:02 -0500

    Add files via upload

---
+++ test/head.test.js
@@ -0,0 +1,29 @@
+
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+// const assert = require('chai').assert;
+
+
+// describe("#head", () => {
+//   it("returns 'a' for ['a', 'b', 'c']", () => {
+//     assert.strictEqual(head(['a', 'b', 'c']), 'a');
+//   });
+
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '5'); 
+//   });
+// });
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([])," ");
+// assertEqual(head([]), );
+// assertEqual(head([7]), 7);
